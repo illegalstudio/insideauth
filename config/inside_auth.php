4d756a55d64662817d37a73021911621188eb6d1
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-29 23:58:59 +0200

    Removed all not necessary configuration variables

--- config/inside_auth.php
+++ config/inside_auth.php
@@ -1,12 +1,7 @@
 <?php
 
 return [
-    'use_inside_auth'    => env('INSIDE_AUTH_USE_INSIDE_AUTH', true),
-    'require_valid_user' => env('INSIDE_AUTH_REQUIRE_VALID_USER', true),
-    'functionalities'    => [
-        'register'           => env('INSIDE_AUTH_FUNCTIONALITIES_REGISTER', true),
-        'forgot_password'    => env('INSIDE_AUTH_FUNCTIONALITIES_FORGOT_PASSWORD', true),
-        'email_verification' => env('INSIDE_AUTH_FUNCTIONALITIES_EMAIL_VERIFICATION', true),
-        'user_profile'       => env('INSIDE_AUTH_FUNCTIONALITIES_USER_PROFILE', true),
-    ]
+    'db' => [
+        'prefix' => env('INSIDE_AUTH_DB_PREFIX', 'auth_'),
+    ],
 ];
