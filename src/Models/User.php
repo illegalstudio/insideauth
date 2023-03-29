2e63525a2130bc863ee73dd70180299385fe8fdf
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-30 00:15:43 +0200

    Fixed check for emailverification enable

--- src/Models/User.php
+++ src/Models/User.php
@@ -119,7 +119,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     public function sendEmailVerificationNotification(): void
     {
-        if (InsideAuth::isEmailVerificationEnabled()) {
+        if (!InsideAuth::current()->isEmailVerificationEnabled()) {
             return;
         }
 
