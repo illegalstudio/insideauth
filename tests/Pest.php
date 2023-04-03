10183385feeeeb5704c85aabd739946479739c19
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-04-03 22:24:25 +0200

    Pest, use custom Testcase for Feature testing

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,7 +11,7 @@
 |
 */
 
-uses(Tests\TestCase::class)->in('Feature');
+uses(Illegal\InsideAuth\Tests\TestCase::class)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
