14ef1286396c4d900ea44f0b2e8056bbfc47a2b8
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-04-03 22:24:10 +0200

    Updated TestCase for Feature testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,43 @@
 <?php
 
-namespace Tests;
+namespace Illegal\InsideAuth\Tests;
 
+use Illegal\InsideAuth\Authenticator;
+use Illegal\InsideAuth\InsideAuth;
+use Illegal\InsideAuth\Providers\ServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * The name of the authentication
+     */
+    protected string $authName = 'test';
+
+    /**
+     * Boot the authentication
+     * @throws \Exception
+     */
+    protected function bootAuth(): void
+    {
+        InsideAuth::boot($this->authName);
+    }
+
+    /**
+     * Returns the Authenticator
+     */
+    protected function auth(): Authenticator
+    {
+        return InsideAuth::getAuthenticator($this->authName);
+    }
+
+    /**
+     * Returns the package providers needed for the tests
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
 }
