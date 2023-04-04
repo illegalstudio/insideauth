bd469338f815053a7c23a513a83390875b108630
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-04-04 23:37:14 +0200

    Re-organized tests

--- tests/Feature/LoggedOutTest.php
+++ tests/Feature/LoggedOutTest.php
@@ -1,96 +1,46 @@
 <?php
 
-use Illegal\InsideAuth\InsideAuth;
+use function Pest\Laravel\{get};
+use function Pest\Laravel\{withoutVite};
 
 beforeEach(
 /**
  * @throws Exception
  */
     function () {
-        $this->withoutVite();
+        withoutVite();
         $this->bootAuth();
     }
 );
 
-/**
- * Check if the login is available
- */
-it('exposes login', function () {
-    $this->get(route($this->auth()->route_login))
-        ->assertOk() // Should be 200
-        ->assertSee('Email') // Email field should be present
-        ->assertSee('Password') // Password field should be present
-        ->assertSee('Forgot your password') // Login button should be present
-        ->assertSee('Register here') // Register link should be present
-    ;
-});
+it('routes to dashboard',               fn() => $this->routes->toDashboard());
+it('routes to login',                   fn() => $this->routes->toLogin());
+it('routes to logout',                  fn() => $this->routes->toLogout());
+it('routes to register',                fn() => $this->routes->toRegister());
+it('routes to password confirm',        fn() => $this->routes->toPasswordConfirm());
+it('routes to password request',        fn() => $this->routes->toPasswordRequest());
+it('routes to password email',          fn() => $this->routes->toPasswordEmail());
+it('routes to password update',         fn() => $this->routes->toPasswordUpdate());
+it('routes to password store',          fn() => $this->routes->toPasswordStore());
+it('routes to password reset',          fn() => $this->routes->toPasswordReset());
+it('routes to profile edit',            fn() => $this->routes->toProfileEdit());
+it('routes to profile update',          fn() => $this->routes->toProfileUpdate());
+it('routes to profile destroy',         fn() => $this->routes->toProfileDestroy());
+it('routes to verification send',       fn() => $this->routes->toVerificationSend());
+it('routes to verification notice',     fn() => $this->routes->toVerificationNotice());
+it('routes to verification verify',     fn() => $this->routes->toVerificationVerify());
 
 /**
- * Check if the registration is available
+ * Check that the login, register, forgot password and reset password are available
  */
-it('exposes register', function () {
-    $this->get(route($this->auth()->route_register))
-        ->assertOk() // Should be 200
-        ->assertSee('Name') // Name field should be present
-        ->assertSee('Email') // Email field should be present
-        ->assertSee('Password') // Password field should be present
-        ->assertSee('Confirm Password') // Confirm Password field should be present
-        ->assertSee('Register') // Register button should be present
-        ->assertSee('Already registered?') // Login link should be present
-    ;
-});
+it('exposes login',             fn() => $this->exposes->login());
+it('exposes register',          fn() => $this->exposes->register());
+it('exposes forgot password',   fn() => $this->exposes->forgotPassword());
+it('exposes reset password',    fn() => $this->exposes->resetPassword());
 
 /**
- * Check if the forgot password is available
+ * Correcly redirects to login protected routes
  */
-it('exposes forgot password', function () {
-    $this->get(route($this->auth()->route_password_request))
-        ->assertOk() // Should be 200
-        ->assertSee('No problem') // No problem text should be present
-        ->assertSee('Email') // Email field should be present
-        ->assertSee('Email Password Reset Link') // Send Password Reset Link button should be present
-    ;
-});
-
-/**
- * Check that user profile is not available
- */
-it('shouldn\'t expose profile', function () {
-    $this->get(route($this->auth()->route_profile_edit))
-        ->assertRedirect(route($this->auth()->route_login));
-
-    $this->get(route($this->auth()->route_profile_update))
-        ->assertRedirect(route($this->auth()->route_login));
-
-    $this->get(route($this->auth()->route_profile_destroy))
-        ->assertRedirect(route($this->auth()->route_login));
-});
-
-/**
- * Reset password page should be available, also with a wrong token
- */
-it('should expose reset password', function () {
-    $this->get(route($this->auth()->route_password_reset, ['token' => 'wrong']))
-        ->assertOk() // Should be 200
-        ->assertSee('Email') // Email field should be present
-        ->assertSee('Password') // Password field should be present
-        ->assertSee('Confirm Password') // Confirm Password field should be present
-        ->assertSee('Reset Password') // Reset Password button should be present
-    ;
-});
-
-/**
- * Verification notice should redirect to login
- */
-it('should redirect verification notice to login', function () {
-    $this->get(route($this->auth()->route_verification_notice))
-        ->assertRedirect(route($this->auth()->route_login));
-});
-
-/**
- * Verification verify should redirect to login if id and hash are wrong
- */
-it('should redirect verification verify to login if id and hash are wrong', function () {
-    $this->get(route($this->auth()->route_verification_verify, ['id' => 1, 'hash' => 'wrong']))
-        ->assertRedirect(route($this->auth()->route_login));
-});
+it('redirects verification notice to login',    fn() => $this->redirects->verificatioNoticeToLogin());
+it('redirects verification verify to login',    fn() => $this->redirects->verificationVerifyToLogin());
+it('redirects profile to login',                fn() => $this->redirects->profileToLogin());
