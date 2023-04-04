0a37c693e33a00c3a2a465f87971e214763fb482
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-04-04 23:36:42 +0200

    Initialize dependencies, renamed class

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -5,14 +5,27 @@ namespace Illegal\InsideAuth\Tests;
 use Illegal\InsideAuth\Authenticator;
 use Illegal\InsideAuth\InsideAuth;
 use Illegal\InsideAuth\Providers\ServiceProvider;
+use Illegal\InsideAuth\Tests\FeatureHelpers\Exposes;
+use Illegal\InsideAuth\Tests\FeatureHelpers\Providers\RouteServiceProvider;
+use Illegal\InsideAuth\Tests\FeatureHelpers\Redirects;
+use Illegal\InsideAuth\Tests\FeatureHelpers\Routes;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
+abstract class FeatureTestCase extends BaseTestCase
 {
     /**
      * The name of the authentication
      */
     protected string $authName = 'test';
+
+    protected Exposes   $exposes;
+    protected Redirects $redirects;
+    protected Routes    $routes;
+
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+    }
 
     /**
      * Boot the authentication
@@ -20,7 +33,10 @@ abstract class TestCase extends BaseTestCase
      */
     protected function bootAuth(): void
     {
-        InsideAuth::boot($this->authName);
+        $authenticator   = InsideAuth::boot($this->authName);
+        $this->exposes   = new Exposes($authenticator);
+        $this->redirects = new Redirects($authenticator);
+        $this->routes    = new Routes($authenticator);
     }
 
     /**
@@ -38,6 +54,7 @@ abstract class TestCase extends BaseTestCase
     {
         return [
             ServiceProvider::class,
+            RouteServiceProvider::class
         ];
     }
 }
